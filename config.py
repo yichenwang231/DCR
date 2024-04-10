c1c73bbaab4ad34d1df75e451011a047caccf003
yichenwang231 <124519927+yichenwang231@users.noreply.github.com>
2024-04-10 09:56:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+
+cifar_10_root = '/media/ubuntu20/D/xuqiyu/datasets/cifar10'
+cifar_100_root = '../datasets/cifar100'
+cub_root = '../datasets/cub'
+aircraft_root = '../datasets/fgvc-aircraft-2013b'
+car_root = '../datasets/cars'
+herbarium_dataroot = '../datasets/herbarium_19'
+imagenet_root = '../datasets/ImageNet'
+
+
+# OSR Split dir
+osr_split_dir = 'data/ssb_splits'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+exp_root = 'dev_outputs' # All logs and checkpoints will be saved here
